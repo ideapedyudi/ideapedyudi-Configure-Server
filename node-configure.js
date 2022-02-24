dc5979fa159946ea27cb4d1eb873a4b4f74a8e6b
Muhammad Wahyudi <ideapedyudi@gmail.com>
2022-02-24 16:38:23 +0700

    node npm configure

---
+++ node-configure.js
@@ -0,0 +1,13 @@
+
+// # install
+// sudo apt update
+// curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh
+// curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+// source ~/.bashrc
+// nvm list-remote
+
+// nvm install v15.6.0
+
+
+// # command
+// node -v
--- npm-configure.md
+++
@@ -1,6 +0,0 @@
-# install
-sudo apt update
-sudo apt install npm
-
-# command
-sudo npm update npm -g
