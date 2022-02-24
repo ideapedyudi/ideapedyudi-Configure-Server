212f4c7041e009ea2a4b8891ca90233f56827ce7
Muhammad Wahyudi <ideapedyudi@gmail.com>
2022-02-24 16:55:00 +0700

    nginx configure

---
+++ nginx-configure.js
@@ -0,0 +1,17 @@
+
+// # install
+
+// sudo apt update
+// sudo apt install nginx
+// sudo ufw app list
+// sudo ufw allow 'Nginx HTTP'
+// sudo ufw status
+
+// # command
+// sudo systemctl status nginx
+// sudo systemctl stop nginx
+// sudo systemctl start nginx
+// sudo systemctl restart nginx
+// sudo systemctl reload nginx
+// sudo systemctl disable nginx
+// sudo systemctl enable nginx
